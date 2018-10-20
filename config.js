09742e35d2ff24b453a22ffbe7f86fd58974024d
sh-user <35073783+sh-user@users.noreply.github.com>
2018-10-21 02:15:29 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 var gsURL = 'https://script.google.com/macros/s/AKfycbzcBzCF1UHLopqIbyHNvfEjMEnpuhjR5lFIM6H-WxX1xiH-dSE/exec',
+server = 'm23.cloudmqtt.com',
+port = 33492,
 user = 'ivtjhrii',
 pass = '_Gnt6HJ2ncka';

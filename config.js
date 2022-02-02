97d68dc7b94bcfba7e5bd4f328824760219e3421
Belotserkovskii Sergei <35073783+sh-user@users.noreply.github.com>
2022-02-02 19:13:16 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
-let gsURL = 'https://script.google.com/macros/s/AKfycbwss3_wuFqwiMznhHdbh6v3y5I6fRaRTKPnxEeg0oSF7IFbmsFh/exec',
+let gsURL = 'https://cors.seregejb.workers.dev/?https://script.google.com/macros/s/AKfycbwss3_wuFqwiMznhHdbh6v3y5I6fRaRTKPnxEeg0oSF7IFbmsFh/exec',
 server = 'm23.cloudmqtt.com',
 port = 33492,
 user = 'ivtjhrii',
-pass = '_Gnt6HJ2ncka';
+pass = '_Gnt6HJ2ncka',
+delta = 12821;
+
